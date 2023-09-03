3bd8c1ad890853e10558381534004462d0065bed
harrysonford <harrysonford@yandex.ru>
2023-09-03 22:42:33 +0300

    Удалены TODO

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ def check_resources(coffee):
     global MENU, resources, READY
     local_coffee = MENU[coffee]
     not_enough = 0
-    # print(f"На выбранный кофе нужны следующие ресурсы: {local_coffee}")
-    # print(f"В машине вот столько всего: {resources}")
-    # print(f"Ингридиенты на подготовку {coffee}: {local_coffee['ingredients']}")
     for key in local_coffee['ingredients']:
         if int(local_coffee['ingredients'][key]) > int(resources[key]):
             not_enough += 1
@@ -28,14 +25,12 @@ def check_resources(coffee):
         READY = True
     return READY
 
-
 # функция по вычету ресурсов
 def minus_resources(coffee):
     global MENU, resources, READY
     local_coffee = MENU[coffee]['ingredients']
     for key in local_coffee:
         resources[key] -= local_coffee[key]
-
 
 # функция по внесению денег
 def insert_cash():
@@ -63,15 +58,11 @@ def check_money(money, coffee):
 # функция кофе машины
 def coffe_machine():
     global QUESTION, resources, CASH, END_MACHINE, MENU
-    # TODO: 5. Если ресурсов хватает, то прошу внести деньги
+    #          Если ресурсов хватает, то прошу внести деньги
     #          Вносятся 50 или 100 или 500
-    # if not READY:
-    #     END_MACHINE = True
-    #     coffe_machine()
-    # else:
     CASH = insert_cash()
 
-    # TODO: 6. Проверка хватает ли денег
+    #          Проверка хватает ли денег
     #          Если денег не хватает - вывести сообщение "Нужно больше денег, деньги возвращены"
     #          Если денег хватает, то цена на кофе вычитается из суммы денег, а сумма добавляется в
     #          Переменную report. Сдача должна выдаваться
@@ -83,7 +74,7 @@ def coffe_machine():
         print(f"Твоя сдача: {CASH-MENU[QUESTION]['cost']} ₽")
         resources['money'] += MENU[QUESTION]['cost']
 
-        # TODO: 7. Сделать кофе.
+        #          Сделать кофе.
         #          Если денег хватает и ресурсы на месте, тогда ресурсы вычитаются из переменной report
         #          После вычитания ресурсов,выдать сообщение "Ваш кофе, сэр!"
         minus_resources(QUESTION)
@@ -91,32 +82,31 @@ def coffe_machine():
 
 
 # основная часть программы
-
 print("Вас приветствует кофемашина Фугарова. Мы предлагаем три вида напитков: эспрессо, латте и капучино.")
 sleep(2)
 
 while not END_MACHINE:
-    # TODO: 1. Ask user "What would you like?" (espresso/latte/cappuccino)
+    #       Ask user "What would you like?" (espresso/latte/cappuccino)
     #       a. Check the user’s input to decide what to do next.
     #       b. The prompt should show every time action has completed, e.g. once the drink is
     #       dispensed. The prompt should show again to serve the next customer.
     QUESTION = input("Чего желаете, мусье? Эспрессо, латте или капучино? "
                      "Посмотреть ресурсы: 'инфо'. Выключить девайс: 'выкл' ").lower()
 
-    # TODO: 2. Turn off the Coffee Machine by entering “off” to the prompt.
+    #           Turn off the Coffee Machine by entering “off” to the prompt.
     #           a. For maintainers of the coffee machine, they can use “off” as
     #           the secret word to turn off the machine. Your code should end execution when this happens.
     if QUESTION == 'выкл':
         END_MACHINE = True
 
-    # TODO: 3. Print report if question is report
+    # Print report if question is report
 
     elif QUESTION == 'инфо':
         print("Сейчас в автомате:")
         for key in resources:
             print(f"{key.capitalize()}: {resources[key]}")
 
-    # TODO: 4. Проверить количество оставшихся ресурсов
+    #          Проверить количество оставшихся ресурсов
     #          когда выбираешь напиток, машина проверяет, хватает ли ресурсов
     #          Если не хватает ресурса - вывести сообщение "Не хватает ресурсов"
     elif QUESTION == 'эспрессо' or QUESTION == 'капучино' or QUESTION == 'латте':
